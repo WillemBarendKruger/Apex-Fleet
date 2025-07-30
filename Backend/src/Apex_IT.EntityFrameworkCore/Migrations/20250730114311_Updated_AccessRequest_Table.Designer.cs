7bbb7add1567143a3fec0d8b97b339520203b2e9
WillemBarendKruger <willem.kruger11@gmail.com>
2025-07-30 13:46:46 +0200

    Bug - fixed type in AccessRequest entity

--- Backend/src/Apex_IT.EntityFrameworkCore/Migrations/20250730114311_Updated_AccessRequest_Table.Designer.cs
+++ Backend/src/Apex_IT.EntityFrameworkCore/Migrations/20250730114311_Updated_AccessRequest_Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Apex_IT.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Apex_IT.Migrations
 {
     [DbContext(typeof(Apex_ITDbContext))]
-    partial class Apex_ITDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250730114311_Updated_AccessRequest_Table")]
+    partial class Updated_AccessRequest_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
