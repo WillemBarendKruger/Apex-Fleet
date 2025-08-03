cd0ea7343484b9f0562d6f5c0dbac24b8dbd8ab5
WillemBarendKruger <willem.kruger11@gmail.com>
2025-08-03 17:19:16 +0200

    Feature - implemeted the request UI

---
+++ Frontend/src/components/list-component/style/styles.ts
@@ -0,0 +1,14 @@
+import { createStyles, css } from "antd-style";
+
+export const useStyles = createStyles({
+    incidentContainer: css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    padding: 24px;
+  `,
+    incidentTable: css`
+    width: 100%;
+  `,
+});
